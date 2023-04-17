51bc4f2b06680fa2aab4c3be098a2592e08fe1e6
Adao Santos <djadao@gmail.com>
2023-04-17 00:05:18 -0300

    initial version

---
+++ src/github.ts
@@ -0,0 +1,21 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {CommitFile} from './types'
+
+export async function getFiles(
+  target: string
+): Promise<(CommitFile | undefined)[]> {
+  const client = github.getOctokit(core.getInput('github_token'))
+
+  const commits = await client.rest.repos.listCommits({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    sha: target
+  })
+
+  if (!commits.data) return []
+
+  const files = commits.data.flatMap(c => c.files)
+
+  return files
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {getFiles} from './github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const target = core.getInput('target') || github.context.sha
+    core.debug(`Commit target => '${target}'`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const pattern = core.getInput('pattern') || '*'
+    core.debug(`Pattern => '${pattern}'`)
 
-    core.setOutput('time', new Date().toTimeString())
+    const files = (await getFiles(target)).filter(f => {
+      if (f === undefined) return false
+
+      return new RegExp(pattern).test(f.filename)
+    })
+
+    core.setOutput('has-changes', files.length > 0)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
